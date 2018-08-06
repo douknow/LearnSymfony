39b37d8a6f84c2fe8d4ca40cc880165f00d682e9
douknow <saynotta@gmail.com>
2018-08-06 11:15:31 +0800

    Add user.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,18 +2,63 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends Controller
 {
     /**
      * @Route("/user/add", name="AddUser")
      */
-    public function addAction(Request $request)
+    public function editAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('user/add.html.twig');
+        // Valid input data.
+        return $this->render("user/add.html.twig");
+    }
+
+    /**
+     * @Route("/user/all", name="allUser")
+     */
+    public function allAction(Request $request)
+    {
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+        return $this->render("user/all.html.twig", array("users" => $users));
+    }
+
+    /**
+     * @Route("/user/submit", name="submitUser")
+     */
+    public function submitAction(Request $request, ValidatorInterface $validator)
+    {
+        $username = $request->get("username");
+        $password = $request->get("password");
+        $phoneNumber = $request->get("phoneNumber");
+        $email = $request->get("email");
+        $sex = $request->get("sex");
+        $birthday = $request->get("birthday");
+        $avatar = $request->get("avatar");
+
+        $user = new User($username, $password, $phoneNumber, $email, $sex, new \DateTime($birthday), $avatar);
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            $messages = "";
+            foreach ($errors as $violation) {
+                $messages .= $violation->getMessage() ."<br>";
+            }
+            return $this->render("user/error.html.twig", Array("messages" => $messages));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirect("all");
     }
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,241 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="用户名不能为空"
+     * )
+     * @Assert\Length(
+     *     min=4,
+     *     max=40,
+     *     minMessage="用户名长度需大于3",
+     *     maxMessage="用户名长度需小于40"
+     * )
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="密码不能为空"
+     * )
+     * @Assert\Length(
+     *     min=8,
+     *     max=20,
+     *     minMessage="密码长度至少8位",
+     *     maxMessage="密码不得超过20位"
+     * )
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="手机号码不能为空"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^1\d{10}$/",
+     *     message="手机号码格式不正确"
+     * )
+     */
+    private $phoneNumber;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="邮箱不能为空"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^[\w]+@[\w]+\.(com|net|cn)+$/",
+     *     message="邮箱格式不正确"
+     * )
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sex;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Assert\NotBlank(
+     *     message="生日不能为空"
+     * )
+     */
+    private $birthday;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $avatar;
+
+    /**
+     * User constructor.
+     * @param $username
+     * @param $password
+     * @param $phoneNumber
+     * @param $email
+     * @param $sex
+     * @param $birthday
+     * @param $avatar
+     */
+    public function __construct($username, $password, $phoneNumber, $email, $sex, $birthday, $avatar)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->phoneNumber = $phoneNumber;
+        $this->email = $email;
+        $this->sex = $sex;
+        $this->birthday = $birthday;
+        $this->avatar = $avatar;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param mixed $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * @param mixed $sex
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @param mixed $birthday
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * @param mixed $avatar
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+    }
+
+
+}
